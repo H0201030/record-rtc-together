62cb241f3e9c2c60736689eeb16fca44f2406ea8
Wang Zhuochun <stone1551@gmail.com>
2013-07-21 01:32:58 +0800

    add grunt concat and uglify

--- gruntfile.js
+++ gruntfile.js
@@ -10,9 +10,34 @@ module.exports = function(grunt) {
                     keepalive: true
                 }
             }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/vendor/whammy.js',
+                      'src/vendor/recorder.js',
+                      'src/js/record-rtc.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('build', ['concat', 'uglify']);
 
 };
